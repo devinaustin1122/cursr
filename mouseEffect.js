e800de3c33b6278ff5a67353a058056a4afc0fa7
dyerda@longwood.edu <dyerda@longwood.edu>
2022-06-21 16:43:39 -0400

    various updates

--- mouseEffect.js
+++ mouseEffect.js
@@ -1,4 +1,4 @@
-function mouse() {
+function mouse(configs) {
   let mouseX = 0;
   let mouseY = 0;
   let divX = 0;
@@ -8,19 +8,29 @@ function mouse() {
   let started = false;
   let background = document.createElement("div");
 
-  let defaults = {
-    delay: 0.5,
-  };
+  // update cursor
+  let html = document.getElementsByTagName("html");
+  html[0].style.cursor = `url("${configs.svg}"), pointer`;
+
+  // update cursor background
+  background.setAttribute("id", "mouse-background");
+  background.style.backgroundImage = `url("${configs.bg}")`;
+  document.body.appendChild(background);
 
   function animate(options) {
+    let defaults = {
+      delay: 1,
+      ...options,
+    };
+
     started = true;
-    updatePosition(0.5);
+    updatePosition(defaults.delay);
     background.style.top = `${divY}px`;
     background.style.left = `${divX}px`;
 
     // if mouse and background are close, end animation.
     if (Math.abs(dX + dY) > 0.1) {
-      requestAnimationFrame(() => animate(options));
+      requestAnimationFrame(() => animate(defaults));
     } else {
       started = false;
     }
@@ -34,11 +44,6 @@ function mouse() {
   }
 
   function effect(options) {
-    options = { ...defaults, ...options };
-
-    background.setAttribute("id", "mouse-background");
-    document.body.appendChild(background);
-
     document.addEventListener("mousemove", (e) => {
       mouseX = e.pageX;
       mouseY = e.pageY;
