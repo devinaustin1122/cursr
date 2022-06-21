28c605d279355d215ed67e4f58741c580d8e5c3c
dyerda@longwood.edu <dyerda@longwood.edu>
2022-06-21 11:50:29 -0400

    various updates

--- mouseEffect.js
+++ mouseEffect.js
@@ -1,24 +1,51 @@
 function mouse() {
-  function effect() {
-    let background = document.createElement("div");
+  let mouseX = 0;
+  let mouseY = 0;
+  let divX = 0;
+  let divY = 0;
+  let dX = 0;
+  let dY = 0;
+  let started = false;
+  let background = document.createElement("div");
+
+  let defaults = {
+    delay: 0.5,
+  };
+
+  function animate(options) {
+    started = true;
+    updatePosition(0.5);
+    background.style.top = `${divY}px`;
+    background.style.left = `${divX}px`;
+
+    // if mouse and background are close, end animation.
+    if (Math.abs(dX + dY) > 0.1) {
+      requestAnimationFrame(() => animate(options));
+    } else {
+      started = false;
+    }
+  }
+
+  function updatePosition(delay) {
+    dX = mouseX - divX + 5 - background.offsetWidth / 2;
+    dY = mouseY - divY + 5 - background.offsetWidth / 2;
+    divX += dX * delay;
+    divY += dY * delay;
+  }
+
+  function effect(options) {
+    options = { ...defaults, ...options };
+
     background.setAttribute("id", "mouse-background");
     document.body.appendChild(background);
 
-    let prevTop = document.pageY - background.offsetHeight / 2;
-    let prevLeft = document.pageX - background.offsetWidth / 2;
-
     document.addEventListener("mousemove", (e) => {
-      let top = e.pageY - background.offsetHeight / 2;
-      let left = e.pageX - background.offsetWidth / 2;
-      background.style.top = `${top}px`;
-      background.style.left = `${left}px`;
-      console.log("prevTop: " + prevTop);
-      console.log("prevLeft: " + prevTop);
-      console.log("top: " + top);
-      console.log("left: " + left);
-
-      prevTop = top;
-      prevLeft = left;
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      // start animation if started is false.
+      if (!started) {
+        animate(options);
+      }
     });
   }
 
