c308b64e9ee9afdebfbc7e71772e8e0b84904276
dyerda@longwood.edu <dyerda@longwood.edu>
2022-06-22 14:04:18 -0400

    update index.html. need to factor out later

--- mouseEffect.js
+++ mouseEffect.js
@@ -1,21 +1,31 @@
-function mouse(configs) {
+function mouse(configs, event) {
   let mouseX = 0;
   let mouseY = 0;
-  let divX = 0;
-  let divY = 0;
+  let divX = event ? event.pageX : 0;
+  let divY = event ? event.pageY : 0;
   let dX = 0;
   let dY = 0;
   let started = false;
-  let background = document.createElement("div");
+  let background = (bg = document.getElementById("mouse-background"))
+    ? bg
+    : document.createElement("div");
+  let html = document.getElementsByTagName("html")[0];
 
-  // update cursor
-  let html = document.getElementsByTagName("html");
-  html[0].style.cursor = `url("${configs.svg}"), pointer`;
+  clear();
+  init();
 
-  // update cursor background
-  background.setAttribute("id", "mouse-background");
-  background.style.backgroundImage = `url("${configs.bg}")`;
-  document.body.appendChild(background);
+  function init() {
+    // update cursor
+    html.style.cursor = `url(${configs.svg}), auto`;
+
+    // update cursor background
+    background.setAttribute("id", "mouse-background");
+    background.style.backgroundImage = configs.bg
+      ? `url("${configs.bg}")`
+      : "unset";
+    document.body.appendChild(background);
+    setPosition(divX, divY);
+  }
 
   function animate(options) {
     let defaults = {
@@ -25,10 +35,8 @@ function mouse(configs) {
 
     started = true;
     updatePosition(defaults.delay);
-    background.style.top = `${divY}px`;
-    background.style.left = `${divX}px`;
 
-    // if mouse and background are close, end animation.
+    // if mouse and background position are close, end animation.
     if (Math.abs(dX + dY) > 0.1) {
       requestAnimationFrame(() => animate(defaults));
     } else {
@@ -36,11 +44,23 @@ function mouse(configs) {
     }
   }
 
+  function setPosition(x, y) {
+    background.style.left = `${x + 5 - background.offsetHeight / 2}px`;
+    background.style.top = `${y + 5 - background.offsetHeight / 2}px`;
+  }
+
   function updatePosition(delay) {
-    dX = mouseX - divX + 5 - background.offsetWidth / 2;
-    dY = mouseY - divY + 5 - background.offsetWidth / 2;
+    dX = mouseX - divX;
+    dY = mouseY - divY;
     divX += dX * delay;
     divY += dY * delay;
+    setPosition(divX, divY);
+  }
+
+  // clear function
+  function clear() {
+    background.remove();
+    html.style.cursor = "auto";
   }
 
   function effect(options) {
