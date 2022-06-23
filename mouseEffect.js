561187d8f2299fa13cd955e984ec2d168171c468
dyerda@longwood.edu <dyerda@longwood.edu>
2022-06-23 17:07:23 -0400

    move, remove and rename files. add extra satelites

--- mouseEffect.js
+++ mouseEffect.js
@@ -1,85 +1,83 @@
-function mouse(configs, event) {
-  let mouseX = 0;
-  let mouseY = 0;
-  let divX = event ? event.pageX : 0;
-  let divY = event ? event.pageY : 0;
-  let dX = 0;
-  let dY = 0;
-  let started = false;
-  let background = (bg = document.getElementById("mouse-background"))
-    ? bg
-    : document.createElement("div");
-  let html = document.getElementsByTagName("html")[0];
+function mouse(wrapper, options) {
+  let canvas = document.createElement("canvas");
+  let context = canvas.getContext("2d");
+  let satellites = [];
+  let cursor = { x: 0, y: 0 };
 
-  //*************************************************************
-  // Need to start thinking about handling errors.
-  // I wonder if there are any functional design patterns
-  //*************************************************************
+  // initialization function
+  function init() {
+    canvas.style.pointerEvents = "none";
 
-  // update cursor if configuration exists
-  html.style.cursor = configs.svg ? `url(${configs.svg}), auto` : "auto";
-
-  // update cursor background img if configuration exists
-  background.setAttribute("id", "mouse-background");
-  background.style.backgroundImage = configs.bg
-    ? `url("${configs.bg}")`
-    : "unset";
-  document.body.appendChild(background);
-
-  //position cursor background
-  setPosition(divX, divY);
-
-  function effect(options) {
-    document.addEventListener("mousemove", (e) => {
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      // start animation if started is false.
-      if (!started) {
-        follow({ delay: 1, ...options });
-      }
-    });
-  }
-
-  function follow(options) {
-    started = true;
-    updatePosition(options.delay);
-
-    // if mouse and background position are close, end animation.
-    // this might be able to be turned into it's own function.
-    if (Math.abs(dX + dY) > 0.1) {
-      requestAnimationFrame(() => follow(options));
+    if (wrapper) {
+      let wrapperElement = document.getElementById("wrapper");
+      canvas.style.position = "absolute";
+      canvas.style.top = `${wrapperElement.offsetTop}px`;
+      canvas.style.left = `${wrapperElement.offsetLeft}px`;
+      canvas.width = wrapperElement.offsetWidth;
+      canvas.height = wrapperElement.offsetHeight;
+      wrapperElement.appendChild(canvas);
     } else {
-      started = false;
+      canvas.style.position = "fixed";
+      canvas.style.top = "0px";
+      canvas.style.left = "0px";
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      document.body.appendChild(canvas);
     }
+
+    document.addEventListener("mousemove", (e) => {
+      cursor.x = e.clientX;
+      cursor.y = e.clientY;
+    });
+
+    satellites.push(new Satellite("./images/circle.svg", 0, 0, 10, 10, 0.2));
+    satellites.push(new Satellite("./images/circle.svg", 0, 0, 10, 10, 0.4));
+    satellites.push(new Satellite("./images/circle.svg", 0, 0, 10, 10, 0.6));
+    satellites.push(new Satellite("./images/circle.svg", 0, 0, 10, 10, 0.8));
+    recurse();
   }
 
-  // might want to use canvas here. might be difficult with event object.
-  // trying to keep everything clean.
-  //
-  // https://www.kirupa.com/canvas/creating_motion_trails.htm
-  function trail() {
-    clone = background.cloneNode();
-    html.appendChild(clone);
+  // recursive function for animations
+  function recurse() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < satellites.length; i++) {
+      satellites[i].update(cursor.x, cursor.y);
+      satellites[i].draw();
+    }
+    requestAnimationFrame(recurse);
   }
 
-  function setPosition(x, y) {
-    background.style.left = `${x + 5 - background.offsetHeight / 2}px`;
-    background.style.top = `${y + 5 - background.offsetHeight / 2}px`;
+  // function used to prerender images
+  function preRender(src) {
+    let canvasOS = document.createElement("canvas");
+    let contextOS = canvasOS.getContext("2d");
+    const image = new Image();
+    image.src = src;
+    image.onload = () => contextOS.drawImage(image, 0, 0);
+    return canvasOS;
   }
 
-  function updatePosition(delay) {
-    dX = mouseX - divX;
-    dY = mouseY - divY;
-    divX += dX * delay;
-    divY += dY * delay;
-    setPosition(divX, divY);
+  // satellite images used to create effects
+  function Satellite(src, x, y, width, height, delay) {
+    this.image = preRender(src);
+    this.x = cursor.x;
+    this.y = cursor.y;
+    this.dx = 0;
+    this.dy = 0;
+    this.height = height;
+    this.width = width;
+
+    this.update = (x, y) => {
+      this.dx = (x - this.x) * delay;
+      this.x += this.dx;
+      this.dy = (y - this.y) * delay;
+      this.y += this.dy;
+    };
+
+    this.draw = () => {
+      context.drawImage(this.image, this.x, this.y);
+    };
   }
 
-  function fade(element) {}
-
-  let self = {
-    effect,
-  };
-
-  return self;
+  init();
 }
