5eee7b55b515c770e6a5ed6fb9fb77e290aa9c53
dyerda@longwood.edu <dyerda@longwood.edu>
2022-07-07 16:30:41 -0400

    update index.js (refactored version)

--- experimental/index.js
+++ experimental/index.js
@@ -2,42 +2,59 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let particle = {
   image: preRender("./ellipse.svg"),
-  x: 0,
-  y: 0,
+  ...pair(0, 0),
 };
-let cursor = {
-  x: 0,
-  y: 0,
-};
-let velocity = {
-  x: 0,
-  y: 0,
-};
+let cursor = pair(0, 0);
+let velocity = pair(0, 0);
+let length = 10;
 
+// initialize
 function init() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
   document.addEventListener("mousemove", async (e) => {
-    cursor = {
-      x: e.clientX,
-      y: e.clientY,
-    };
+    cursor = pair(e.clientX, e.clientY);
   });
+
+  trail();
 }
 
-// it's working somewhat, but I don't want there to be any pulling when
-// shape is close to cursor. Need to add free fall effect.
-function loop() {
+// animation recursive loop
+// add update function parameter
+function elastic() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  velocity = subtract(cursor, particle);
-  console.log(velocity);
+
+  let tmp = subtract(cursor, particle);
+
+  velocity.x = velocity.x * 0.9 + (tmp.x / length) * 0.2;
+  velocity.y = velocity.y * 0.9 + (tmp.y / length) * 0.2;
+
   particle.x = particle.x + velocity.x;
   particle.y = particle.y + velocity.y;
+
   ctx.drawImage(particle.image, particle.x, particle.y);
-  requestAnimationFrame(loop);
+  requestAnimationFrame(elastic);
 }
 
+// animation recursive loop
+// add update function parameter
+function trail() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  let velocity = subtract(cursor, particle);
+
+  velocity.x = velocity.x * 0.1;
+  velocity.y = velocity.y * 0.1;
+
+  particle.x = particle.x + velocity.x;
+  particle.y = particle.y + velocity.y;
+
+  ctx.drawImage(particle.image, particle.x, particle.y);
+  requestAnimationFrame(trail);
+}
+
+// prerenders the image. improves performance
 function preRender(src) {
   let canvasOS = document.createElement("canvas");
   let ctxOS = canvasOS.getContext("2d");
@@ -47,23 +64,22 @@ function preRender(src) {
   return canvasOS;
 }
 
+// subtracts two pairs
 function subtract(first, second) {
-  return {
-    x: first.x - second.x,
-    y: first.y - second.y,
-  };
+  return pair(first.x - second.x, first.y - second.y);
 }
 
-function add(first, second) {
-  return {
-    x: first.x + second.x,
-    y: first.y + second.y,
-  };
+// using the component form, gets magnitude of vector
+function magnitude(component) {
+  return Math.sqrt(Math.pow(component.x, 2) + Math.pow(component.y, 2));
 }
 
-// function distance(first, second) {
-//   // calculate the distance
-// }
+// factory function for a pair object
+function pair(x, y) {
+  return {
+    x,
+    y,
+  };
+}
 
 init();
-loop();
