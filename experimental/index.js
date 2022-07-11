c78f6585c06d6ef251882df84ac22eea96ffce76
Devin Dyer <dyerda@longwood.edu>
2022-07-11 16:16:49 -0400

    more experimental updates

--- experimental/index.js
+++ experimental/index.js
@@ -1,112 +1,114 @@
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
-let particles = [particle("./ellipse.svg")];
+let particles = [];
 let cursor = pair(0, 0);
 let length = 10;
 
-// initialize everything
+// initialize document and canvas
 function init() {
-  // this needs to be updated
   canvas.style.position = "fixed";
   canvas.style.top = "0px";
   canvas.style.left = "0px";
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-}
 
-// function prepares and returns all relevant effects
-function follow() {
-  init();
-  document.body.appendChild(canvas);
   document.addEventListener("mousemove", (e) => {
     cursor = pair(e.clientX, e.clientY);
   });
 
-  return {
-    trail,
-    elastic,
-  };
-}
-
-// function prepares and returns all relevant effects
-function spawn() {
-  init();
   document.body.appendChild(canvas);
-  document.addEventListener("mousemove", (e) => {
-    addParticle(pair(e.clientX, e.clientY), 100);
-  });
-
-  return {
-    float,
-  };
 }
 
-// add particle to particles list
-function addParticle(coordinates, limit) {
-  let particle;
-  if (particles.length > limit) {
-    particle = particles.pop();
-  } else {
-    particle = {
-      image: preRender("./ellipse.svg"),
-      opacity: 1,
-      ...coordinates,
-    };
+// initalize the effect
+function effect(configs) {
+  init();
+  particles.push(particle("./ellipse.svg", cursor));
+  // loop(elastic);
+  // loop(trail);
+  let spawn = true;
+  if (spawn) {
+    document.addEventListener("mousemove", (e) => {
+      add(particle("./ellipse.svg", cursor));
+    });
   }
+  loop(float, shrink);
+}
 
-  particles.unshift({
-    ...particle,
-    opacity: 1,
-    ...coordinates,
+// animation loop function
+function loop(...updates) {
+  updates.forEach((update) => {
+    update(particles);
+  });
+  draw(particles);
+  requestAnimationFrame(() => loop(...updates));
+}
+
+// draw function
+function draw(particles) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach((particle) => {
+    ctx.globalAlpha = particle.opacity;
+    ctx.drawImage(
+      particle.image,
+      particle.x,
+      particle.y,
+      particle.image.width * particle.scale,
+      particle.image.height * particle.scale
+    );
   });
 }
 
-// floating effect
-function float() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].y -= 1;
-    particles[i].opacity *= 0.9;
-    ctx.globalAlpha = particles[i].opacity;
-    ctx.drawImage(particles[i].image, particles[i].x, particles[i].y);
-  }
-  requestAnimationFrame(float);
+// add particle
+function add(particle, limit) {
+  particles.unshift(particle);
+  console.log(particles);
 }
 
-// animation recursive loop
-// add update function parameter
-function elastic() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+// elastic update function
+function elastic(particles) {
+  particles.forEach((particle) => {
+    let spring = subtract(cursor, particle);
 
-  let spring = subtract(cursor, particles[0]);
+    particle.velocity.x = particle.velocity.x * 0.9 + (spring.x / length) * 0.2;
+    particle.velocity.y = particle.velocity.y * 0.9 + (spring.y / length) * 0.2;
 
-  particles[0].velocity.x =
-    particles[0].velocity.x * 0.9 + (spring.x / length) * 1;
-  particles[0].velocity.y =
-    particles[0].velocity.y * 0.9 + (spring.y / length) * 1;
-
-  particles[0].x = particles[0].x + particles[0].velocity.x;
-  particles[0].y = particles[0].y + particles[0].velocity.y;
-
-  ctx.drawImage(particles[0].image, particles[0].x, particles[0].y);
-  requestAnimationFrame(elastic);
+    particle.x = particle.x + particle.velocity.x;
+    particle.y = particle.y + particle.velocity.y;
+  });
 }
 
-// animation recursive loop
-// add update function parameter
-function trail() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+// trail update function
+function trail(particles) {
+  particles.forEach((particle) => {
+    let velocity = subtract(cursor, particle);
 
-  let velocity = subtract(cursor, particles[0]);
+    velocity.x = velocity.x * 0.1;
+    velocity.y = velocity.y * 0.1;
 
-  velocity.x = velocity.x * 0.1;
-  velocity.y = velocity.y * 0.1;
+    particle.x = particle.x + velocity.x;
+    particle.y = particle.y + velocity.y;
+  });
+}
 
-  particles[0].x = particles[0].x + velocity.x;
-  particles[0].y = particles[0].y + velocity.y;
+// float update function
+function float(particles) {
+  particles.forEach((particle) => {
+    particle.y -= 1;
+  });
+}
 
-  ctx.drawImage(particles[0].image, particles[0].x, particles[0].y);
-  requestAnimationFrame(trail);
+// function fade
+function fade(particles) {
+  particles.forEach((particle) => {
+    particle.opacity *= 0.9;
+  });
+}
+
+// function shrink
+function shrink(particles) {
+  particles.forEach((particle) => {
+    particle.scale *= 0.99;
+  });
 }
 
 // subtracts two pairs
@@ -114,9 +116,16 @@ function subtract(first, second) {
   return pair(first.x - second.x, first.y - second.y);
 }
 
-// using the component form, gets magnitude of vector
-function magnitude(component) {
-  return Math.sqrt(Math.pow(component.x, 2) + Math.pow(component.y, 2));
+// prerenders the image. improves performance.
+function render(src) {
+  let canvasOS = document.createElement("canvas");
+  canvasOS.width = 100;
+  canvasOS.height = 100;
+  let ctxOS = canvasOS.getContext("2d");
+  const image = new Image();
+  image.src = src;
+  image.onload = () => ctxOS.drawImage(image, 0, 0);
+  return canvasOS;
 }
 
 // factory function for a pair object
@@ -127,23 +136,13 @@ function pair(x, y) {
   };
 }
 
-// factory functino for particles
-function particle(src) {
+// factory function for particles
+function particle(src, coordinates) {
   return {
-    image: preRender(src),
+    image: render(src),
     opacity: 1,
     velocity: pair(0, 0),
-    ...pair(0, 0),
+    scale: 1,
+    ...coordinates,
   };
-}
-
-// utility functions
-// prerenders the image. improves performance
-function preRender(src) {
-  let canvasOS = document.createElement("canvas");
-  let ctxOS = canvasOS.getContext("2d");
-  const image = new Image();
-  image.src = src;
-  image.onload = () => ctxOS.drawImage(image, 0, 0);
-  return canvasOS;
 }
---
+++ idea.js
@@ -0,0 +1,7 @@
+// const defaults
+
+// factory and composition function for creating particles
+
+// bind appropriate follow/spawn function to onmousemove
+
+Mouse({ selector: "myDiv", cursor: "cursor.svg" }).follow({ img: "img.svg" });
